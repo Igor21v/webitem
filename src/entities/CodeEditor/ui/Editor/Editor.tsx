e9dbbf5b458e70e968574bd24a4345bacc9abd9f
Igor Bondarenko <igor21v@mail.ru>
2023-02-24 21:23:58 +0800

    Not lazy import lib

--- src/entities/CodeEditor/ui/Editor/Editor.tsx
+++ src/entities/CodeEditor/ui/Editor/Editor.tsx
@@ -1,12 +1,8 @@
-import React, {
-    Dispatch,
-    memo,
-    SetStateAction,
-    useEffect,
-    useRef,
-} from 'react';
+import React, { Dispatch, memo, SetStateAction } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
-import { LanguageSupport } from '@codemirror/language';
+import { html } from '@codemirror/lang-html';
+import { css } from '@codemirror/lang-css';
+import { javascript } from '@codemirror/lang-javascript';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Editor.module.scss';
 import { ContentType, languageType } from '../CodeEditor/CodeEditor';
@@ -18,55 +14,27 @@ interface EditorProps {
     setContent: Dispatch<SetStateAction<ContentType>>;
 }
 
-type htmlType = typeof import('@codemirror/lang-html');
-type cssType = typeof import('@codemirror/lang-css');
-type javascriptType = typeof import('@codemirror/lang-javascript');
-const getAsyncCodeModules = async () => {
-    return Promise.all([
-        import('@codemirror/lang-html'),
-        import('@codemirror/lang-css'),
-        import('@codemirror/lang-javascript'),
-    ]);
-};
-
 export const Editor = memo((props: EditorProps) => {
     const { className, openedEditor, content, setContent } = props;
     const handleChange = (value: string | undefined) => {
         setContent({ ...content, [openedEditor]: value });
     };
 
-    const htmlRef = useRef<htmlType>();
-    const cssRef = useRef<cssType>();
-    const javascriptRef = useRef<javascriptType>();
-    useEffect(() => {
-        getAsyncCodeModules().then(([html, css, javascript]) => {
-            htmlRef.current = html;
-            cssRef.current = css;
-            javascriptRef.current = javascript;
-        });
-    }, []);
-    let currLang: (() => LanguageSupport) | undefined;
+    let currLang;
     let currContent;
     switch (openedEditor) {
         case 'html':
-            currLang = htmlRef.current?.html;
+            currLang = html;
             currContent = content.html;
             break;
         case 'css':
-            currLang = cssRef.current?.css;
+            currLang = css;
             currContent = content.css;
             break;
         default:
-            currLang = javascriptRef.current?.javascript;
+            currLang = javascript;
             currContent = content.js;
     }
-
-    const getLang = () => {
-        if (currLang) {
-            return [currLang()];
-        }
-        return [];
-    };
 
     return (
         <CodeMirror
@@ -75,7 +43,7 @@ export const Editor = memo((props: EditorProps) => {
             value={currContent}
             minHeight="200px"
             theme="dark"
-            extensions={getLang()}
+            extensions={[currLang()]}
         />
     );
 });
