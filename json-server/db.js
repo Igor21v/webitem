eae537bc7ba239a3113331cc852e809d72cafeb6
Igor Bondarenko <igor21v@mail.ru>
2023-03-07 16:04:32 +0800

    destructuring item in BD

--- json-server/db.js
+++ json-server/db.js
@@ -1,9 +1,9 @@
 const comments = require('./db/comments.json');
 const itemRatings = require('./db/itemRatings.json');
-const items = require('./db/items.json');
 const notifications = require('./db/notifications.json');
 const profile = require('./db/profile.json');
 const users = require('./db/users.json');
+const items = require('./db/items')();
 
 module.exports = function () {
     return {
