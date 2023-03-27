ef54b0403beeb3584b3e84e54abe6f234fea47ee
Igor Bondarenko <igor21v@mail.ru>
2023-03-27 10:26:10 +0800

    add slider

--- json-server/db/items.js
+++ json-server/db/items.js
@@ -2,7 +2,8 @@ const alert = require('./items/alert.json');
 const accordion = require('./items/accordion.json');
 const animation = require('./items/animation.json');
 const loader = require('./items/loader.json');
+const slider = require('./items/slider.json');
 
 module.exports = function () {
-    return [...alert, ...accordion, ...animation, ...loader];
+    return [...alert, ...accordion, ...animation, ...loader, ...slider];
 };
