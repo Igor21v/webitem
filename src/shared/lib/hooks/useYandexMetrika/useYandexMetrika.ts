0e8255b3af0860f79fcc4e3b3fb03e4b7566707d
Igor Bondarenko <igor21v@mail.ru>
2023-06-02 11:29:29 +0800

    deleted debug logs

--- src/shared/lib/hooks/useYandexMetrika/useYandexMetrika.ts
+++ src/shared/lib/hooks/useYandexMetrika/useYandexMetrika.ts
@@ -8,8 +8,6 @@ export function useYandexMetrikaHit(deps?: string) {
             !__IS_DEV__
         ) {
             ym(93784203, 'hit', '#');
-            console.log('Trap YM');
         }
-        console.log(`Trap YM 2${deps}`);
     }, [deps]);
 }
