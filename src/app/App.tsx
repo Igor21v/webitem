c410ede72426ebe8a583f499e1315301c4fff086
Igor Bondarenko <igor21v@mail.ru>
2023-06-12 21:29:27 +0800

    try use useLangeInit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,6 +15,7 @@ export default function App() {
     }, [dispatch]);
     const inited = useSelector(getUserInited);
     const { isScreenXl } = useResizeWindow();
+    /* useLangInit(); */
 
     return (
         <div className={classNames('app', {}, [])}>
---
+++ src/shared/lib/hooks/useLangInit/useLangInit.ts
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const useLangInit = () => {
+    const { i18n } = useTranslation();
+    const url = new URL(String(document.location));
+    const langPath = url.pathname.slice(1, 3);
+
+    useEffect(() => {
+        if (langPath === 'en') {
+            if (i18n.language !== 'en') {
+                i18n.changeLanguage('en');
+            }
+        } else if (i18n.language !== 'ru') {
+            i18n.changeLanguage('ru');
+        }
+    }, [i18n, langPath]);
+    return null;
+};
