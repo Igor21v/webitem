b3cf722eded4d2287eca27c05a8a3f0e2e5a39e2
Igor Bondarenko <igor21v@mail.ru>
2023-07-30 18:28:38 +0800

    fix storybook

--- src/pages/ItemsPage/ui/ItemsPageFilters/ItemsPageFilters.stories.tsx
+++ src/pages/ItemsPage/ui/ItemsPageFilters/ItemsPageFilters.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator';
 import { ItemsPageFilters } from './ItemsPageFilters';
+import { RouterDecorator } from '@/shared/config/storybook/RouterDecorator';
 
 export default {
     title: 'pages/ItemsPage/ItemsPageFilters',
@@ -8,7 +9,7 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-    decorators: [StoreDecorator({})],
+    decorators: [StoreDecorator({}), RouterDecorator()],
 } as ComponentMeta<typeof ItemsPageFilters>;
 
 const Template: ComponentStory<typeof ItemsPageFilters> = (args) => (
