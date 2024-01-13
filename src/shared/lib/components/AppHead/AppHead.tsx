728ffc42f5981fa9a532ce0535e1a74e3bdae9d6
Igor Bondarenko <igor21v@mail.ru>
2024-01-13 21:09:31 +0800

    seo: add link whith hrefLang

--- src/shared/lib/components/AppHead/AppHead.tsx
+++ src/shared/lib/components/AppHead/AppHead.tsx
@@ -1,4 +1,5 @@
 import { memo } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Portal } from '../../../ui/Portal';
 
 interface AppHeadProps {
@@ -9,9 +10,14 @@ interface AppHeadProps {
 
 export const AppHead = memo((props: AppHeadProps) => {
     const { title, description, keywords } = props;
+    const { pathname } = useLocation();
+    const fullPathRu = `https://webitem.ru${pathname}`;
+    const fullPathEn = `https://webitem.ru/en${pathname}`;
     return (
         <Portal element={document.head}>
             <title>{title}</title>
+            <link rel="alternate" hrefLang="ru" href={fullPathRu} />
+            <link rel="alternate" hrefLang="en" href={fullPathEn} />
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
         </Portal>
