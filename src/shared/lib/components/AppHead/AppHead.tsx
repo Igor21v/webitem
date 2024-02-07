8cc8dff12866e885b249173e071160df15daa873
Igor Bondarenko <igor21v@mail.ru>
2024-02-07 22:28:24 +0800

    fix: path to alternate link for seo en/en/

--- src/shared/lib/components/AppHead/AppHead.tsx
+++ src/shared/lib/components/AppHead/AppHead.tsx
@@ -1,4 +1,5 @@
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 import { Portal } from '../../../ui/Portal';
 
@@ -10,7 +11,11 @@ interface AppHeadProps {
 
 export const AppHead = memo((props: AppHeadProps) => {
     const { title, description, keywords } = props;
-    const { pathname } = useLocation();
+    let { pathname } = useLocation();
+    const { i18n } = useTranslation();
+    if (i18n.language === 'en') {
+        pathname = pathname.slice(3);
+    }
     const fullPathRu = `https://webitem.ru${pathname}`;
     const fullPathEn = `https://webitem.ru/en${pathname}`;
     return (
